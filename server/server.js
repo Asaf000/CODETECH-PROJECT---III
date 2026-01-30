a1e33b82410f83b855c553bc3f8bb0d596369aed
Asaf000 <asafeqbal6@gmail.com>
2026-01-30 22:33:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/collab-editor';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/CODETECH PROJECT - III';
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
